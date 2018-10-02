9f2725f37c46b7dc30e630c3d2cff13d871243e3
weilinix <weilinix@outlook.com>
2018-10-02 23:59:35 +0800

    Add post-create component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { PostCreateComponent } from './posts/post-create/post-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostCreateComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/posts/post-create/post-create.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-post-create',
+  templateUrl: './post-create.component.html'
+})
+export class PostCreateComponent {
+  enteredValue = '';
+  newPost = 'NO CONTENT';
+
+  // onAddPost(postInput: HTMLTextAreaElement) {
+  //   this.newPost = postInput.value;
+  // }
+
+  onAddPost() {
+    this.newPost = this.enteredValue;
+  }
+}
